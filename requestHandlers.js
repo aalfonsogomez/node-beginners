5a686ed73e11fa93bce8fcd78debf9ee4276121b
Angel <a.alfonso.gomez@gmail.com>
2014-12-12 12:29:23 +0100

    Next step

--- requestHandlers.js
+++ requestHandlers.js
@@ -2,11 +2,20 @@ var exec = require("child_process").exec;
 
 function iniciar(response) {
 	console.log("Manipulador de petición 'iniciar' ha sido llamado.");
-	exec("ls -lah", function ( error, stdout, stderr) {
-		response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(stdout);
-    response.end();
-  });
+	var body = '<html><head>'+
+    '<meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/subir" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Submit text" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+	response.writeHead(200, {"Content-Type": "text/html"});
+  response.write(body);
+  response.end();
 }
 
 function subir(response) {
